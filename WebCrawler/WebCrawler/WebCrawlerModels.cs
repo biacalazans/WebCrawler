e0a3063a22923460fa7b1114e6beaea6f11f5a8f
biacalazans <biacalazans@users.noreply.github.com>
2020-09-15 12:44:40 -0300

    Iniciando projeto

---
+++ WebCrawler/WebCrawler/WebCrawlerModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCrawler
+{
+    public class Autor
+    {
+        public long AutorId { get; set; }
+
+        public string Nom { get; set; }
+
+        public long LivroId { get; set; }
+
+        public Livro Livros { get; set; }
+    }
+
+    public class Livro
+    {
+        public long LivroId { get; set; }
+
+        public string Titulo { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public ICollection<Autor> Autor { get; set; }
+    }
+}
